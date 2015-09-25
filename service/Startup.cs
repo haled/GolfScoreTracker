e8e96fdbdb9edda8cab47703436ec6ef9fdfcc4d
Darren Hale <darrenhale@darrenhale.clearent.lan>
2015-09-25 14:09:28 -0500

    Upgraded the project to work with DNX Beta7.  The back-end service works, but the Angular app doesn't.

--- service/Startup.cs
+++ service/Startup.cs
@@ -16,26 +16,16 @@ namespace service
         {
         }
 
-        // This method gets called by a runtime.
-        // Use this method to add services to the container
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
-            // Uncomment the following line to add Web API services which makes it easier to port Web API 2 controllers.
-            // You will also need to add the Microsoft.AspNet.Mvc.WebApiCompatShim package to the 'dependencies' section of project.json.
-            // services.AddWebApiConventions();
         }
 
-        // Configure is called after ConfigureServices is called.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+      public void Configure(IApplicationBuilder app)
         {
-            // Configure the HTTP request pipeline.
             app.UseStaticFiles();
 
-            // Add MVC to the request pipeline.
             app.UseMvc();
-            // Add the following route for porting Web API 2 controllers.
-            // routes.MapWebApiRoute("DefaultApi", "api/{controller}/{id?}");
         }
     }
 }
