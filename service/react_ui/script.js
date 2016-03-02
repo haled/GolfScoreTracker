866ea9b8ac17a575ae58f7f77366f536fe511e41
Darren Hale <darrenhale@darrenhale.clearent.lan>
2016-03-02 08:51:41 -0600

    Updated some of the API references to work with RC1.  Also added a ReactJS playground area.

---
+++ service/react_ui/script.js
@@ -0,0 +1,15 @@
+// To get started with this tutorial running your own code, simply remove
+// the script tag loading scripts/example.js and start writing code here.
+var CommentBox = React.createClass({
+  render: function() {
+    return (
+      <div className="commentBox">
+        Hello, world! I am a CommentBox.
+      </div>
+    );
+  }
+});
+ReactDOM.render(
+  <CommentBox />,
+  document.getElementById('content')
+);
